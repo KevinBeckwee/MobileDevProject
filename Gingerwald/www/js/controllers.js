50717ca7f1f249a9a614eeacf3415eff3a3858e9
KevinBeckwee <medabots4@hotmail.com>
2016-12-06 17:03:54 +0100

    files with qr-code

--- Gingerwald/www/js/controllers.js
+++ Gingerwald/www/js/controllers.js
@@ -25,7 +25,31 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
-})
+});
+
+.controller('ScanController', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform) {
+        var vm = this;
+
+        vm.scan = function(){
+            $ionicPlatform.ready(function() {
+                $cordovaBarcodeScanner
+                    .scan()
+                    .then(function(result) {
+                        // Success! Barcode data is here
+                        vm.scanResults = "We got a barcode\n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled;
+                    }, function(error) {
+                        // An error occurred
+                        vm.scanResults = 'Error: ' + error;
+                    });
+            });
+        };
+
+        vm.scanResults = '';
+    });
+
 
 .controller('LoginCtrl', function ($scope) {
   $scope.data = {};
