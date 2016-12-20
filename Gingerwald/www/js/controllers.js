369c09f446cd93b6c0acd8421431105a74f27052
JordyMous <jordymous@outlook.com>
2016-12-20 16:02:31 +0100

    Update
    Onnodige templates verwijderd, login link nu naar 'menu', menu is niet
    de laatste versie atm

--- Gingerwald/www/js/controllers.js
+++ Gingerwald/www/js/controllers.js
@@ -1,63 +1,20 @@
     angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-})
-
 .controller('LoginCtrl', function ($scope, LoginService, $ionicPopup, $state) {
   $scope.data = {};
 
   $scope.login = function () {
-    LoginService.loginUser ($scope.data.username, $scope.data.password).success (function (data) {
-      $state.go ('tab.dash');
+     LoginService.loginUser ($scope.data.username, $scope.data.password).success (function (data) {
+      $state.go ('menu');
     }).error (function (data) {
       var alertPopup = $ionicPopup.alert ({
         title: 'Login failed',
         template: 'Please check your credentials!'
       });
     });
-
-    //console.log ('LOGIN --- User: ' + $scope.data.username + ' Pass: ' + $scope.data.password);
   }
 })
 
-.controller('AppCtrl', function($scope) {
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
-    
+.controller('MenuCtrl', function($scope) {
+  //MENUCONTROLLER HIER
+});
--- Gingerwald/www/js/services.js
+++ Gingerwald/www/js/services.js
@@ -1,64 +1,25 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+.service('LoginService', function($q) {
+    return {
+        loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+            if (name == 'user' && pw == 'secret') {
+                deferred.resolve('Welcome ' + name + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
         }
-      }
-      return null;
     }
-  };
 })
-
-.service('LoginService', function($q, $http) {
-  return {
-    loginUser: function(name, pw) {
-      var deferred = $q.defer();
-      var promise = deferred.promise;
-
-      //Tijdelijke logindetails
-      //var getUserDetails = 'https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=';
-      //var userToken = 'VRYsLhjqom93MPPPcfeaWwmb8S3hwS7rImoqS3OOVthP4BFApUPT1wIsW2UmSiFO'; //token moet later nog uit de server gehaald worden adhv van de logingegevens
-
-      //1. token aanvragen adhv van usernaam en wachtwoord (client_secret heb ik er vast ingezet)
-      var url = 'https://www.gingerwald.com/community/v2.1/authorization/oauth/token.php?grant_type=password&username=' + name + '&password=' + pw + '&client_id=GingerwaldUserApp13&client_secret=nO7mudLAY6w2CnNWEv0RNVjsRqfnsh8NYdc4Jpj3Z0PPTvbG1SGihD7w8YedIPtV';
-
-      $http ({
-        method: 'POST',
-        url: url,
-      }).then (function successCallback (response) {
-        console.log (response);
-      }, function errorCallback (response) {
-        console.log (response);
-      });
-
-      //2. indien de token opgehaald kan worden kan men inloggen en worden de gegevens van de gebruikers getoond glinkt aan zijn token
-
-      /*if (name == 'plantijn004@gingerwald.be' && pw == 'gingerjuice') {
-        deferred.resolve('Welcome ' + name + '!');
-      } else {
-        deferred.reject('Wrong credentials.');
-      }
-      promise.success = function(fn) {
-        promise.then(fn);
-        return promise;
-      }
-      promise.error = function(fn) {
-        promise.then(null, fn);
-        return promise;
-      }
-      return promise;*/
-    }
-  }
-});
